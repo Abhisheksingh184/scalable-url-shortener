5a8bbea6899d5cd5a0ac74e160eb56e9462c4741
Abhishek Singh <142322970+Abhisheksingh184@users.noreply.github.com>
2026-05-20 23:50:43 +0530

    chore:initialize spring boot app and project structure

---
+++ src/test/java/scalable_url_shortener/ScalableUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package scalable_url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalableUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
